06b72c8b86b9d1e1a5052d9e8fb6d20e98b4db7c
Geralt from Rivia <ctvbxm@mail.ru>
2021-03-16 21:55:54 +0200

    статус

--- main.py
+++ main.py
@@ -30,12 +30,6 @@ class UI_Main(QMainWindow, Ui_MainWindow):
             """Select id, FIO, sex, email, phone_number from UserForm""").fetchall()
         self.update_table()
 
-        for i in range(1, self.tableWidget.rowCount() + 1):
-            self.anketa = self.curs.execute(
-                f"""Select phone_number, photo_path, campus, place_of_birth, birthday from Anket where id='{i}'""").fetchone()
-
-        print(self.anketa)
-
     def update_table(self):
         self.tableWidget.setRowCount(0)
 
@@ -53,6 +47,32 @@ class UI_Main(QMainWindow, Ui_MainWindow):
             self.tableWidget.item(i, 2).setText(str(self.data[i][2]))
             self.tableWidget.item(i, 3).setText(self.data[i][3])
             self.tableWidget.item(i, 4).setText(str(self.data[i][4]))
+
+        for i in range(n):
+            self.anketa = self.curs.execute(
+                f"""Select Address, Paper, PersonalData, Education, agree_path, edu_form, branch, Exams, Achives, birthday, place_of_birth, phone_number, photo_path, campus, copy, agree_join_path from Anket where id='{i + 1}'""").fetchone()
+
+            kol = 0
+
+            if self.anketa:
+                for j in self.anketa:
+                    if j:
+                        kol += 1
+
+                if kol == len(self.anketa):
+                    self.student_status = 'Полный комплект'
+                elif ((kol == len(self.anketa) - 1) or (kol == len(self.anketa) - 1)) and (
+                        (not self.anketa[-1]) or (not self.anketa[-2])):
+                    self.student_status = 'Комплект без оригинала'
+                elif kol > 0:
+                    self.student_status = 'В работе'
+                else:
+                    self.student_status = 'Новый'
+            else:
+                self.student_status = 'Новый'
+
+            self.tableWidget.setItem(i, 5, QTableWidgetItem())
+            self.tableWidget.item(i, 5).setText(self.student_status)
 
     def student(self):
         try:
