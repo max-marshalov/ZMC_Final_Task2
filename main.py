0d735aa3974ac13fc44470c45b4ca9795f0e46c7
Geralt from Rivia <ctvbxm@mail.ru>
2021-03-16 22:08:21 +0200

    вход

--- main.py
+++ main.py
@@ -3,7 +3,56 @@ from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sqlite3
 from startwindow import Ui_MainWindow
+from join import *
 import sys
+
+
+class Join(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_Join()
+        self.ui.setupUi(self)
+
+        self.ui.label_error.hide()
+
+        self.ui.btn_join.clicked.connect(self.go_join)
+
+    def go_join(self):
+        Login = None
+        Password = None
+
+        if len(self.ui.edit_login.text()) > 0:
+            Login = self.ui.edit_login.text()
+        else:
+            self.ui.label_error.setText("Введите логин и пароль")
+            self.ui.label_error.show()
+            return
+
+        if len(self.ui.edit_password.text()) > 0:
+            Password = self.ui.edit_password.text()
+        else:
+            self.ui.label_error.setText("Введите логин и пароль")
+            self.ui.label_error.show()
+            return
+
+        con = sqlite3.connect("DATABASE.db")
+        curs = con.cursor()
+        ex = curs.execute(
+            """SELECT * FROM UserForm WHERE email = "{}" and password = "{}" """.format(Login, Password)).fetchall()
+        con.commit()
+        con.close()
+        if not ex:
+            self.ui.label_error.setText("Неверный логин или пароль")
+            self.ui.label_error.show()
+            return
+        else:
+            try:
+                self.win = UI_Main(self)
+                self.close()
+                self.win.show()
+
+            except Exception as er:
+                print(er)
 
 
 class UI_Main(QMainWindow, Ui_MainWindow):
@@ -87,6 +136,6 @@ class UI_Main(QMainWindow, Ui_MainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    mainWindow = UI_Main("DATABASE.db")
+    mainWindow = Join()
     mainWindow.show()
     sys.exit(app.exec())
